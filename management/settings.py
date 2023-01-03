e1472a5650451ce5df05be004b9ab1e071f8a8cd
Sylvain <58688278+SylvainDMonkey@users.noreply.github.com>
2023-01-03 09:45:44 +0900

    Reverted secret key to original

--- management/settings.py
+++ management/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-z-h!*_shg=15$roy-oj#(aqa18drqh)n=2p%(su)&guriolu%9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == '1' #1 == True
