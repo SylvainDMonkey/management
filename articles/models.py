ed0a0a308542f94e82b050ddb4de1c442fe9c37a
Sylvain <58688278+SylvainDMonkey@users.noreply.github.com>
2023-01-07 17:37:12 +0900

    Added some tests cases in test.py from articles app

--- articles/models.py
+++ articles/models.py
@@ -29,17 +29,17 @@ class Article(models.Model):
 
         
 def article_pre_save(sender, instance, *args, **kwargs):
-    print('pre_save')
-    print(args, kwargs)
-    print(sender, instance)
+    # print('pre_save')
+    # print(args, kwargs)
+    # print(sender, instance)
     if instance.slug is None:
         slugify_instance_title(instance, save=False)
 
 pre_save.connect(article_pre_save, sender=Article)
 
 def article_post_save(sender, instance, created, *args, **kwargs):
-    print('post_save')
-    print(args, kwargs)
+    # print('post_save')
+    # print(args, kwargs)
     if created:
         slugify_instance_title(instance, save=True)
     
--- articles/utils.py
+++ articles/utils.py
@@ -12,7 +12,7 @@ def slugify_instance_title(instance, save=False, new_slug=None):
     qs = Klass.objects.filter(slug=slug).exclude(id=instance.id)
     if qs.exists():
         #auto generate new slug
-        rand_int = random.randint(0, 500_000)
+        rand_int = random.randint(0, 25_000_000)
         slug = f"{slug}-{rand_int}"
         return slugify_instance_title(instance, save=save, new_slug=slug)
     instance.slug = slug
