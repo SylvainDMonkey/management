663e30790d4d29b19be4db593d3cd90552c01d23
Sylvain <58688278+SylvainDMonkey@users.noreply.github.com>
2023-01-13 17:55:11 +0900

    Modified forms.py from recipes to prepare css

--- recipes/forms.py
+++ recipes/forms.py
@@ -4,9 +4,30 @@ from django import forms
 from .models import Recipe, RecipeIngredient
 
 class RecipeForm(forms.ModelForm):
+    error_css_class = 'error-field'
+    required_css_class = 'required-field'
+    # To add css
+    # name = forms.CharField(label='', widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Recipe name"}))
+    # description = forms.CharField(widget=forms.Textarea(attrs={"rows": 3}))
     class Meta:
         model = Recipe
         fields = ['name', 'description', 'directions']
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            #print(field)
+            new_data = {
+                "placeholder": f"Recipe {str(field)}",
+                "class": 'form-control'
+            }
+            self.fields[str(field)].widget.attrs.update(
+                new_data
+            )
+        #self.fields['name'].label = ''
+        # self.fields['name'].widget.attrs.update({"class": "form-control", "placeholder": "Recipe name"})
+        self.fields['description'].widget.attrs.update({"rows": 2})
+        self.fields['directions'].widget.attrs.update({"rows": 4})
         
         
 class RecipeIngredientForm(forms.ModelForm):
