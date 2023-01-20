1592845f38ed517c194ca86b1eb7e0550eb09275
Sylvain <58688278+SylvainDMonkey@users.noreply.github.com>
2023-01-21 06:16:53 +0900

    Added methods to parse brut texts with OCR

--- recipes/utils.py
+++ recipes/utils.py
@@ -1,4 +1,8 @@
 from fractions import Fraction
+from typing import List
+from pint import UnitRegistry
+
+"""Parsing a text"""
 
 def number_str_to_float(amount_str:str) -> (any, bool):
     """
@@ -7,7 +11,6 @@ def number_str_to_float(amount_str:str) -> (any, bool):
     Valid string returns:
     Float
     Boolean -> True
-
     Invalid string Returns
     Original String
     Boolean -> False
@@ -26,3 +29,64 @@ def number_str_to_float(amount_str:str) -> (any, bool):
     if isinstance(number_as_float, float):
         success = True
     return number_as_float, success
+
+
+
+def parse_paragraph_to_recipe_line(paragraph):
+    paragraph = paragraph.replace("\n", " ").replace("\f", " ").replace("\t", " ")
+    results = []
+    current_str = ""
+    for line in paragraph.split(" "):
+        val, success = number_str_to_float(line)
+        if success:
+            # print(line, val)
+            if current_str != "":
+                results.append(current_str.strip())
+            current_str = f"{line}"
+        else:
+            current_str += f" {line}"
+    return results
+
+
+
+def convert_to_qty_units(results: List[str]):
+    ureg = UnitRegistry()
+    dataset = []
+    for i, x in enumerate(results):
+        words = x.split(" ")
+        qty = None
+        qty_raw = None
+        units = None
+        other = []
+        for word in words:
+            val, success = number_str_to_float(word)
+            if success:
+                qty = val
+                qty_raw = word
+                continue
+            iter_unit = None
+            try:
+                iter_unit = str(ureg[word.lower()].units)
+            except:
+                pass
+            if units is None and iter_unit is not None:
+                units = iter_unit
+            else:
+                other.append(word)
+        name = ""
+        other_txt = " ".join(other)
+        description = None
+        if len(other_txt) < 220:
+            name = other_txt
+        elif len(other_txt) >= 220:
+            name = other_txt[:220]
+            description = other_txt[220:]
+        data = {
+            "quantity_as_float": qty,
+            "quantity": qty_raw,
+            "unit": units,
+            "name": name,
+            "description": description,
+        }
+        dataset.append(data)
+    return dataset
